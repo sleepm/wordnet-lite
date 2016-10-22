6f90441dbd27d78f09336739531b461f2705f829
sleepm <1520667045@qq.com>
2016-10-22 22:24:34 +0800

    update api

--- api/index.php
+++ api/index.php
@@ -4,4 +4,7 @@ header("Content-Type: application/json");
 if(isset($_GET['word'])){    
     $word = urldecode($_GET['word']);
     echo word($word);
+}else if(isset($_GET['length'])&&is_numeric($_GET['length'])){
+    $length = $_GET['length'];
+    echo wordList($length);
 }
